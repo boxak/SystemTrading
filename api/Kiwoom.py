95898ff40d63dece9a9698fe75e807d2fa961b56
boxak <boxak@naver.com>
2021-11-18 05:44:14 +0900

    2021-11-18 kiwoom api writing...

---
+++ api/Kiwoom.py
@@ -0,0 +1,46 @@
+from PyQt5.QAxContainer import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class Kiwoom(QAxWidget):
+    def __init__(self):
+        super().__init__()
+        self._make_kiwoom_instance()
+        self._set_signal_slots()
+        self._comm_connect()
+
+    def _make_kiwoom_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    def _set_signal_slots(self):
+        self.OnEventConnect.connect(self._login_slot)
+
+    def _login_slot(self, err_code):
+        if err_code == 0:
+            print("connected")
+        else:
+            print("not connected")
+
+        self.login_event_loop.exit()
+
+    def _comm_connect(self):
+        self.dynamicCall("CommConnect()")
+
+        self.login_event_loop = QEventLoop()
+
+        self.login_event_loop.exec_()
+
+    def get_account_number(self, tag="ACCNO"):
+        account_list = self.dynamicCall("GetLoginInfo(QString",
+                                        tag)
+        account_number = account_list.split(';')[0]
+        print(account_number)
+
+        return account_number
+
+    def get_code_list_by_market(self, market_type):
+        code_list = self.dynamicCall("GetCodeListByMarket(QString)",
+                                     market_type)
+        code_list = code_list.split(';')[:-1]
+
+        return code_list
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from api.Kiwoom import *
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = QApplication(sys.argv)
+kiwoom = Kiwoom()
+kiwoom.get_account_number()
 
+kospi_code_list = kiwoom.get_code_list_by_market("0")
+print(kospi_code_list)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app.exec_()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
